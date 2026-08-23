c5a555b5e1eaa2d002d52cdce839fcd3e73b76e9
Emilio Beyer-Shelton <EmilioB-S@Hotmail.com>
2026-08-23 20:41:11 +0200

    update prod base url

--- src/router.tsx
+++ src/router.tsx
@@ -2,13 +2,16 @@ import { createBrowserRouter } from "react-router"
 import WorkPlanManager from "./pages/WorkPlanManager"
 import BoundingBoxEditor from "./pages/BoundingBoxEditor"
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <WorkPlanManager />,
-  },
-  {
-    path: "/editor",
-    element: <BoundingBoxEditor />,
-  },
-])
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <WorkPlanManager />,
+    },
+    {
+      path: "/editor",
+      element: <BoundingBoxEditor />,
+    },
+  ],
+  { basename: "/DPM/" }
+)
